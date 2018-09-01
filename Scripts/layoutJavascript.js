7bfb28f70c755bc39c4322e17e3ac0e08bb62458
ianokam <41764978+ianokam@users.noreply.github.com>
2018-09-01 06:36:36 -0400

    fixed locking of navigation when pressed

--- Scripts/layoutJavascript.js
+++ Scripts/layoutJavascript.js
@@ -1,37 +1,64 @@
 /*---------------------------------------------------------------------*/
 /* ---------- [[[    MENU TOGGLE ON/OFF    ]]] ------------------------*/
+var toggleValue=0;
+
+        // un-lock scroll position
+        var html = jQuery('html');
+        var scrollPosition = html.data('scroll-position');
+
+        // // lock scroll position, but retain settings for later
+        // var 
+        // var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+
+
+
 
 $( "button" ).click(function() {
     var win = $(this); //this = window
     var size=$( "body" ).height()+30;
+    
+    toggleValue += 1;
+    if(toggleValue>=2){
+        toggleValue=0;
+    }
+
     $("#menuBarBlack").css("height", size + "px");
 
-    $( ".pos-f-t" ).fadeToggle( "slow", "linear" );
+    $( "#menuBarBlack" ).fadeToggle( "slow", "linear" );
+/*-------------------------------------------------*/    
+    // var i = $( ".p-4" ).css( "top" ); /*Comes out as a string. A number + "px".  (i.e. 70px) */
+    // var numberCache = parseInt( i.substr(0, i.length-2) ); /*Holds just the number. BUT IT IS A STRING - be mindful!!! */
+    // $(".unactiveTab").append(numberCache);
+
+    $( ".p-4" ).slideToggle( "fast" );
+
 /*-------------------------------------------------*/
     var color = $( "button" ).css( "background-color" ); /*color returnd in RGB*/
 
     if(color=="rgb(134, 134, 134)"){ /*turn off*/
         $(".button").css('background-color', "rgb(56, 56, 56)")
 
-        // // un-lock scroll position
-        // var html = jQuery('html');
-        // var scrollPosition = html.data('scroll-position');
-        // html.css('overflow', html.data('previous-overflow'));
-        // window.scrollTo(scrollPosition[0], scrollPosition[1])
+        // un-lock scroll position
+        scrollPosition = html.data('scroll-position');
+
+
+        html.css('overflow', html.data('previous-overflow'));
+        window.scrollTo(scrollPosition[0], scrollPosition[1])
     }
     else if(color== "rgb(56, 56, 56)"){ /*turn on*/
         $(".button").css('background-color', "rgb(134, 134, 134)")
 
-        // // lock scroll position, but retain settings for later
-        // var scrollPosition = [
-        //     self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        //     self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-        // ];
-        // var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-        // html.data('scroll-position', scrollPosition);
-        // html.data('previous-overflow', html.css('overflow'));
-        // html.css('overflow', 'hidden');
-        // window.scrollTo(scrollPosition[0], scrollPosition[1]);
+        // lock scroll position, but retain settings for later
+        scrollPosition = [
+            self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+            self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+        ];
+
+
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
     }    
   });
 /*------------------------------------------------------*/
@@ -40,7 +67,10 @@ $( "button" ).click(function() {
     var size=$( "body" ).height()+30;
     $("#menuBarBlack").css("height", size + "px");
 
-    $( ".pos-f-t" ).fadeToggle( "slow", "linear" );
+    $( "#menuBarBlack" ).fadeToggle( "slow", "linear" );
+
+    /*-------------------------------------------------*/
+    $( ".p-4" ).slideToggle( "fast" );
 
     /*-------------------------------------------------*/
     var color = $( "button" ).css( "background-color" ); /*color returnd in RGB*/
@@ -48,29 +78,64 @@ $( "button" ).click(function() {
     if(color=="rgb(134, 134, 134)"){ /*turn off*/
         $(".button").css('background-color', "rgb(56, 56, 56)")
 
-        // // un-lock scroll position
-        // var html = jQuery('html');
-        // var scrollPosition = html.data('scroll-position');
-        // html.css('overflow', html.data('previous-overflow'));
-        // window.scrollTo(scrollPosition[0], scrollPosition[1])
+        // un-lock scroll position
+        scrollPosition = html.data('scroll-position');
+
+        
+        html.css('overflow', html.data('previous-overflow'));
+        window.scrollTo(scrollPosition[0], scrollPosition[1])
     }
     else if(color== "rgb(56, 56, 56)"){ /*turn on*/
         $(".button").css('background-color', "rgb(134, 134, 134)")
 
-        // // lock scroll position, but retain settings for later
-        // var scrollPosition = [
-        //     self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        //     self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-        // ];
-        // var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-        // html.data('scroll-position', scrollPosition);
-        // html.data('previous-overflow', html.css('overflow'));
-        // html.css('overflow', 'hidden');
-        // window.scrollTo(scrollPosition[0], scrollPosition[1]);
+        // lock scroll position, but retain settings for later
+        scrollPosition = [
+            self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+            self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+        ];
+
+
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
     }  
     
   });
   
+
+  ///////////////////////////////////////////////////
+  // LOCK/UNLOCK SCREEN WHILE RESIZING WINDOW : 
+  ///////////////////////////////////////////////////
+
+    $(window).on('resize', function(){
+
+        var color2 = $( "button" ).css( "background-color" );
+
+        if( ($( window ).width() <= 862 ) && color2=="rgb(134, 134, 134)"  ){
+            $("#menuBarBlack").css("display","initial");
+        
+        // // lock scroll position, but retain settings for later
+        //(NO NEED TO REPEAT THE WHOLE CODE LIKE ABOVE)          //
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+        }
+
+
+        if(  (   $( window ).width() > 862 )   ){
+            $("#menuBarBlack").css("display","none");
+            $(".p-4").css("display","none");
+        
+            // un-lock scroll position
+        scrollPosition = html.data('scroll-position');
+
+        html.css('overflow', html.data('previous-overflow'));
+        window.scrollTo(scrollPosition[0], scrollPosition[1])
+        }
+
+    });
+
+
 /*-------------------------------------------------------*/  
 /* ---------- [[[    TABS    ]]] ------------------------*/
 function openHome() {
